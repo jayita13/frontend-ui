ca67512b0aef7796fb81ea619d0132623d0958ef
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2021-11-16 00:39:20 +0530

    Update card.js

--- card.js
+++ card.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".our-team").on("click", function () {
-    $(this).toggleClass("flipped");
+  $(".flip-card").on("click", function () {
+    $('.our-team').toggleClass("flipped");
   });
 });
