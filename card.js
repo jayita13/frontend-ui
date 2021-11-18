565f3298c6de5b077fca6e6b31dd4556e489d6b0
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2021-11-18 23:52:45 +0530

    Update card.js

--- card.js
+++ card.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
+  // card-flipping
   $(".flip-card").on("click", function () {
     $('.our-team').toggleClass("flipped");
   });
+
+  // date-picker
+  $('#datepicker').datepicker({
+    weekStart: 1,
+    daysOfWeekHighlighted: "6,0",
+    autoclose: true,
+    todayHighlight: true,
+  });
+  $('#datepicker').datepicker("setDate", new Date());
+
+  // filter content acc to dropdown list
+  $('.select-list').on('change', function () {
+    var text = $(this).find('option').filter(':selected').text();
+    $('.our-team').each(function () {
+      if ($(this).text().indexOf(text) != -1) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    })
+  });   
+
 });
